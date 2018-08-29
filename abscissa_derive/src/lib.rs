a11a4f8a1a037ffeb49d41926d8ac1fc8cf2868a
Tony Arcieri <tony@iqlusion.io>
2018-08-29 07:51:19 -0700

    abscissa_derive v0.0.2

--- abscissa_derive/src/lib.rs
+++ abscissa_derive/src/lib.rs
@@ -18,7 +18,7 @@
 #![recursion_limit = "1024"]
 #![doc(
     html_logo_url = "https://www.iqlusion.io/img/github/iqlusioninc/abscissa/abscissa-sq.svg",
-    html_root_url = "https://docs.rs/abscissa_derive/0.0.1"
+    html_root_url = "https://docs.rs/abscissa_derive/0.0.2"
 )]
 
 extern crate proc_macro;
