fa2eb9a80d4bb09f8a0dc852736ea0c543479506
Tony Arcieri <tony@iqlusion.io>
2019-06-17 17:35:42 -0700

    v0.1.0-pre2

--- src/lib.rs
+++ src/lib.rs
@@ -94,7 +94,7 @@
 #![forbid(unsafe_code)]
 #![doc(
     html_logo_url = "https://www.iqlusion.io/img/github/iqlusioninc/abscissa/abscissa-sq.svg",
-    html_root_url = "https://docs.rs/abscissa/0.1.0-pre.1"
+    html_root_url = "https://docs.rs/abscissa/0.1.0-pre.2"
 )]
 
 /// Abscissa version
