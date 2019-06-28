eb62ffc0ecc23d90cc4ffa68fffc792134ac306b
Tony Arcieri <tony@iqlusion.io>
2019-06-28 08:09:55 -0700

    testing: Support for capturing/asserting on streams
    Adds support for interacting with stdin/stdout/stderr as part of
    acceptance tests.

--- src/testing.rs
+++ src/testing.rs
@@ -4,10 +4,12 @@ use crate::error::{
     FrameworkError,
     FrameworkErrorKind::{ProcessError, TimeoutError},
 };
+use regex::Regex;
 use std::{
     ffi::OsString,
-    io::{self, Write},
-    process::{Child, Command},
+    io::{self, BufRead, BufReader, Write},
+    ops::{Deref, DerefMut},
+    process::{Child, ChildStderr, ChildStdin, ChildStdout, Command, Stdio},
     time::Duration,
 };
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
@@ -27,6 +29,12 @@ pub struct CargoRunner {
 
     /// How long to wait until a child exits
     timeout: Duration,
+
+    /// Capture standard output to a pipe
+    capture_stdout: bool,
+
+    /// Capture standard error to a pipe
+    capture_stderr: bool,
 }
 
 impl Default for CargoRunner {
@@ -35,6 +43,8 @@ impl Default for CargoRunner {
             cmd: "cargo".into(),
             args: vec![],
             timeout: Duration::from_secs(DEFAULT_TIMEOUT_SECS),
+            capture_stdout: false,
+            capture_stderr: false,
         }
     }
 }
@@ -76,16 +86,42 @@ impl CargoRunner {
         self
     }
 
+    /// Enable capturing of standard output
+    pub fn capture_stdout(&mut self) -> &mut Self {
+        self.capture_stdout = true;
+        self
+    }
+
+    /// Enable capturing of standard error
+    pub fn capture_stderr(&mut self) -> &mut Self {
+        self.capture_stderr = true;
+        self
+    }
+
     /// Run the given subcommand
     pub fn run(&self) -> Result<Process, FrameworkError> {
         self.print_command().unwrap();
 
-        let child = Command::new(&self.cmd).args(&self.args).spawn()?;
+        let stdout = if self.capture_stdout {
+            Stdio::piped()
+        } else {
+            Stdio::inherit()
+        };
 
-        Ok(Process {
-            child,
-            timeout: self.timeout,
-        })
+        let stderr = if self.capture_stderr {
+            Stdio::piped()
+        } else {
+            Stdio::inherit()
+        };
+
+        let child = Command::new(&self.cmd)
+            .args(&self.args)
+            .stdin(Stdio::piped())
+            .stdout(stdout)
+            .stderr(stderr)
+            .spawn()?;
+
+        Ok(Process::new(child, self.timeout))
     }
 
     /// Get the exit status for the given subcommand
@@ -125,6 +161,12 @@ pub struct CmdRunner {
 
     /// Timeout after which command should complete.
     timeout: Option<Duration>,
+
+    /// Capture standard output to a pipe
+    capture_stdout: bool,
+
+    /// Capture standard error to a pipe
+    capture_stderr: bool,
 }
 
 impl CmdRunner {
@@ -138,6 +180,8 @@ impl CmdRunner {
             bin: Some(bin.into()),
             args: vec![],
             timeout: None,
+            capture_stdout: false,
+            capture_stderr: false,
         }
     }
 
@@ -168,6 +212,18 @@ impl CmdRunner {
         self
     }
 
+    /// Enable capturing of standard output
+    pub fn capture_stdout(&mut self) -> &mut Self {
+        self.capture_stdout = true;
+        self
+    }
+
+    /// Enable capturing of standard error
+    pub fn capture_stderr(&mut self) -> &mut Self {
+        self.capture_stderr = true;
+        self
+    }
+
     /// Invoke `cargo run` with the given arguments
     pub fn run(&self) -> Result<Process, FrameworkError> {
         let mut runner = CargoRunner::default();
@@ -184,6 +240,14 @@ impl CmdRunner {
         if !self.args.is_empty() {
             runner.arg("--");
             runner.args(&self.args);
+        }
+
+        if self.capture_stdout {
+            runner.capture_stdout();
+        }
+
+        if self.capture_stderr {
+            runner.capture_stderr();
         }
 
         runner.run()
@@ -203,9 +267,52 @@ pub struct Process {
 
     /// Timeout after which process should complete
     timeout: Duration,
+
+    /// Standard output (if captured)
+    stdout: Option<Stdout>,
+
+    /// Standard error (if captured)
+    stderr: Option<Stderr>,
+
+    /// Standard input
+    stdin: ChildStdin,
 }
 
 impl Process {
+    /// Create a process from the given `Child`.
+    ///
+    /// This gets invoked from `CargoRunner::run`
+    fn new(mut child: Child, timeout: Duration) -> Self {
+        let stdout = child.stdout.take().map(Stdout::new);
+        let stderr = child.stderr.take().map(Stderr::new);
+        let stdin = child.stdin.take().unwrap();
+        Self {
+            child,
+            timeout,
+            stdout,
+            stderr,
+            stdin,
+        }
+    }
+
+    /// Gets a handle to the child's stdout.
+    ///
+    /// Panics if the child's stdout isn't captured (via `capture_stdout`)
+    pub fn stdout(&mut self) -> &mut Stdout {
+        self.stdout
+            .as_mut()
+            .expect("child stdout not captured (use 'capture_stdout' method)")
+    }
+
+    /// Gets a handle to the child's stderr.
+    ///
+    /// Panics if the child's stderr isn't captured (via `capture_stderr`)
+    pub fn stderr(&mut self) -> &mut Stderr {
+        self.stderr
+            .as_mut()
+            .expect("child stderr not captured (use 'capture_stderr' method)")
+    }
+
     /// Wait for the child to exit
     pub fn wait(mut self) -> Result<ExitStatus, FrameworkError> {
         match self.child.wait_timeout(self.timeout)? {
@@ -223,6 +330,84 @@ impl Process {
         }
     }
 }
+
+impl Write for Process {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.stdin.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.stdin.flush()
+    }
+}
+
+/// Buffered reader for standard output
+#[derive(Debug)]
+pub struct Stdout(BufReader<ChildStdout>);
+
+/// Buffered reader for standard error
+#[derive(Debug)]
+pub struct Stderr(BufReader<ChildStderr>);
+
+macro_rules! impl_output_stream {
+    ($name:tt, $inner:ty) => {
+        impl $name {
+            /// Create standard output wrapper
+            fn new(stream: $inner) -> $name {
+                $name(BufReader::new(stream))
+            }
+
+            /// Read a line and ensure it matches the expected value.
+            ///
+            /// Panics if it is not the expected value.
+            pub fn expect_line(&mut self, expected_line: &str) {
+                let mut actual_line = String::new();
+                self.0.read_line(&mut actual_line).unwrap_or_else(|e| {
+                    panic!("error reading line from {}: {}", stringify!($name), e)
+                });
+
+                assert_eq!(expected_line, actual_line.trim_end_matches('\n'));
+            }
+
+            /// Read a line and test it against the given regex.
+            ///
+            /// Panics if the line does not match the regex.
+            pub fn expect_regex(&mut self, regex: &str) {
+                let r = Regex::new(regex)
+                    .unwrap_or_else(|e| panic!("error compiling regex {:?}: {}", regex, e));
+
+                let mut line = String::new();
+                self.0.read_line(&mut line).unwrap_or_else(|e| {
+                    panic!("error reading line from {}: {}", stringify!($name), e)
+                });
+
+                assert!(
+                    r.is_match(line.trim_end_matches('\n')),
+                    "regex {:?} did not match line: {:?}",
+                    regex,
+                    line
+                );
+            }
+        }
+
+        impl Deref for $name {
+            type Target = BufReader<$inner>;
+
+            fn deref(&self) -> &BufReader<$inner> {
+                &self.0
+            }
+        }
+
+        impl DerefMut for $name {
+            fn deref_mut(&mut self) -> &mut BufReader<$inner> {
+                &mut self.0
+            }
+        }
+    };
+}
+
+impl_output_stream!(Stdout, ChildStdout);
+impl_output_stream!(Stderr, ChildStderr);
 
 /// Information about a process's exit status
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
@@ -242,11 +427,20 @@ impl ExitStatus {
     }
 
     /// Assert that the process exited successfully
-    pub fn assert_success(&self) {
+    pub fn expect_success(&self) {
         assert_eq!(
-            self.code, 0,
+            0, self.code,
             "process exited with error status: {}",
             self.code
         );
+    }
+
+    /// Assert that the process exited with the given code
+    pub fn expect_code(&self, code: i32) {
+        assert_eq!(
+            code, self.code,
+            "process exited with status code: {} (expected {})",
+            self.code, code
+        )
     }
 }
---
+++ tests/app/exit_status.rs
@@ -0,0 +1,22 @@
+//! Tests for different exit status codes for different usage patterns
+
+use abscissa::testing::CmdRunner;
+
+#[test]
+fn no_args() {
+    CmdRunner::default()
+        .capture_stdout()
+        .status()
+        .unwrap()
+        .expect_success();
+}
+
+#[test]
+fn invalid_args() {
+    CmdRunner::default()
+        .arg("foobar") // invalid arg
+        .capture_stdout()
+        .status()
+        .unwrap()
+        .expect_code(101);
+}
