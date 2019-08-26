69482714833e6b44ef1cc1e73c09670943d22a8c
Tony Arcieri <tony@iqlusion.io>
2019-08-26 12:58:07 -0700

    cli/template: Lint for 2018 edition idioms; export error macros
    Adds a `rust_2018_idioms` lint to the default template.
    
    Re-exports the `abscissa_core::{ensure, fail, fatal}` macros from the
    generated app's prelude.

--- cli/src/prelude.rs
+++ cli/src/prelude.rs
@@ -4,5 +4,8 @@
 /// Commonly used Abscissa traits
 pub use abscissa_core::{Application, Command, Runnable};
 
+/// Error macros
+pub use abscissa_core::error::macros::{};
+
 /// Logging macros
 pub use abscissa_core::log::{debug, error, info, log, log_enabled, trace, warn};
