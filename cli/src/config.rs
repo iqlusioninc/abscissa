c4fd88ff6e13a5061f1ed6d9ff7f894397dcccd9
Tony Arcieri <tony@iqlusion.io>
2019-11-16 11:46:31 -0800

    Use blanet impl for Config
    Replaces a (somewhat silly) use of custom derive using a blanket impl

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -1,10 +1,9 @@
 //! Abscissa CLI Config
 
-use abscissa_core::config::Config;
 use serde::{Deserialize, Serialize};
 
 /// Abscissa CLI Config
-#[derive(Config, Clone, Debug, Default, Deserialize, Serialize)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 pub struct CliConfig {
     // TODO(tarcieri): configuration file?
