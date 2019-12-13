05de8fc3912a66f62be12aaa678208a5640f51a4
Tony Arcieri <tony@iqlusion.io>
2019-12-12 16:37:38 -0800

    Split core prelude from template
    Moving parts of the prelude into the framework itself eliminates the
    need to update boilerplate whenever the prelude changes.

--- cli/src/prelude.rs
+++ cli/src/prelude.rs
@@ -1,14 +1,5 @@
 //! Application-local prelude: conveniently import types/functions/macros
 //! which are generally useful and should be available everywhere.
 
-/// Commonly used Abscissa traits
-pub use abscissa_core::{Application, Command, Runnable};
-
-/// Error macros
-pub use abscissa_core::{ensure, fail, fatal, format_err};
-
-/// Tracing macros
-pub use abscissa_core::tracing::{debug, error, event, info, span, trace, warn, Level};
-
-/// Status macros
-pub use abscissa_core::{status_err, status_info, status_ok, status_warn};
+/// Abscissa core prelude
+pub use abscissa_core::prelude::*;
