12646f6935c2511b08e2e639e265f088e93eb29c
Tony Arcieri <tony@iqlusion.io>
2020-02-10 14:11:13 -0800

    abscissa_tokio v0.5.0
    Initial release

--- tokio/src/lib.rs
+++ tokio/src/lib.rs
@@ -91,7 +91,7 @@
 
 #![doc(
     html_logo_url = "https://www.iqlusion.io/img/github/iqlusioninc/abscissa/abscissa-sq.svg",
-    html_root_url = "https://docs.rs/abscissa_tokio/0.5.0-pre"
+    html_root_url = "https://docs.rs/abscissa_tokio/0.5.0"
 )]
 #![forbid(unsafe_code)]
 #![warn(rust_2018_idioms, unused_lifetimes, unused_qualifications)]
