63a60b32565b1dfa92b572b16c1eb5a65365c682
Tony Arcieri (iqlusion) <tony@iqlusion.io>
2020-12-27 17:01:59 -0800

    core: fine-grained application state locking (#425)
    Removes the `RwLock` on application state (previously located in
    `AppCell`), replacing it with a fine-grained locking strategy:
    
    - Makes the `Application` type immutable with various interior
      mutability strategies
    - Uses `arc-swap` to handle updatable `Config` storage, as mediated
      through a new `CfgCell` wrapper
    - Makes the `application::State` struct opaque with `RwLock` access to
      inner fields mediated by method-based accessors
    - Adds type aliases for state accessors to make changing the strategy
      simpler in the future

--- cli/src/application.rs
+++ cli/src/application.rs
@@ -3,6 +3,7 @@
 use super::{commands::CliCommand, config::CliConfig};
 use abscissa_core::{
     application::{AppCell, State},
+    config::{self, CfgCell},
     trace, Application, EntryPoint, FrameworkError, StandardPaths,
 };
 
@@ -10,22 +11,13 @@ use abscissa_core::{
 pub static APP: AppCell<CliApplication> = AppCell::new();
 
 /// Abscissa CLI Application
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct CliApplication {
     /// Application configuration.
-    config: Option<CliConfig>,
+    config: CfgCell<CliConfig>,
 
     /// Application state.
     state: State<Self>,
-}
-
-impl Default for CliApplication {
-    fn default() -> Self {
-        Self {
-            config: None,
-            state: Default::default(),
-        }
-    }
 }
 
 impl Application for CliApplication {
@@ -33,26 +25,24 @@ impl Application for CliApplication {
     type Cfg = CliConfig;
     type Paths = StandardPaths;
 
-    fn config(&self) -> &CliConfig {
-        self.config.as_ref().expect("config not loaded")
+    fn config(&self) -> config::Reader<CliConfig> {
+        self.config.read()
     }
 
     fn state(&self) -> &State<Self> {
         &self.state
     }
 
-    fn state_mut(&mut self) -> &mut State<Self> {
-        &mut self.state
-    }
-
     fn register_components(&mut self, command: &Self::Cmd) -> Result<(), FrameworkError> {
-        let components = self.framework_components(command)?;
-        self.state.components.register(components)
+        let framework_components = self.framework_components(command)?;
+        let mut app_components = self.state.components_mut();
+        app_components.register(framework_components)
     }
 
     fn after_config(&mut self, config: Self::Cfg) -> Result<(), FrameworkError> {
-        self.state.components.after_config(&config)?;
-        self.config = Some(config);
+        let mut components = self.state.components_mut();
+        components.after_config(&config)?;
+        self.config.set_once(config);
         Ok(())
     }
 
