31bdcedea9dcee4777f3000a9a99751a57b987c1
Shella Stephens <shella@iqlusion.io>
2023-09-21 15:11:00 -0600

    Use Rust 2021 Edition in `abscissa new` (#856)

--- cli/src/properties/rust.rs
+++ cli/src/properties/rust.rs
@@ -2,10 +2,10 @@
 
 use serde::{Deserialize, Serialize};
 
-/// Rust edition used by the application (2018+)
+/// Rust edition used by the application (2021+)
 #[derive(Copy, Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Edition {
-    /// Rust 2018 edition (minimum supported by Abscissa)
-    #[serde(rename = "2018")]
-    Rust2018,
+    /// Rust 2021 edition (minimum supported by Abscissa)
+    #[serde(rename = "2021")]
+    Rust2021,
 }
